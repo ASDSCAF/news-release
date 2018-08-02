c19f8917f9121c5d0a81e260be7a409e4a92f477
ASD <1031239695@qq.com>
2018-08-02 21:00:43 +0800

    第二次测试修改

--- src/org/jbit/news/service/NewstServicDao.java
+++ src/org/jbit/news/service/NewstServicDao.java
@@ -18,8 +18,6 @@ public interface NewstServicDao {
 	int deleteTop(int tid) throws SQLException;
 	int addTop(String tname) throws SQLException;
 	int updateTop(Topic topic) throws SQLException;
-
-	//ssssss
 	
 	News_users login(News_users nu) throws SQLException;
 	
